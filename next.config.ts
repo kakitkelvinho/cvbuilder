b96d41de0cf7e19a96509e71f7649dc0cc19ef70
Kelvin <zcapkkk@ucl.ac.uk>
2025-06-20 18:29:14 +0200

    added output: export

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   basePath: isProd ? '/cvbuilder' : '',
+  output: 'export',
 };
 
 export default nextConfig;
