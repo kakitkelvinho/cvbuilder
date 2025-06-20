9f348575ea7ce2d7679d228f1cfab07990bd07ae
Kelvin <zcapkkk@ucl.ac.uk>
2025-06-20 18:55:15 +0200

    next step is to try hanging indent

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   basePath: isProd ? '/cvbuilder' : '',
   output: 'export',
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
